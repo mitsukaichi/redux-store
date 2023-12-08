58bf9c320bb251e5fb55eba792ac578ae6e423b3
Minami Itsukaichi <mitukaichi@gmail.com>
2023-12-07 17:43:52 -0800

    refactor the context api to redux

---
+++ client/src/components/CategoryMenu/index.jsx
@@ -0,0 +1,67 @@
+import { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import  store  from '../../utils/GlobalState';
+import {
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+} from '../../utils/actions';
+import { QUERY_CATEGORIES } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
+
+function CategoryMenu() {
+
+  const { categories } = store.getState();
+
+  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+  useEffect(() => {
+    if (categoryData) {
+      store.dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories,
+      });
+      categoryData.categories.forEach((category) => {
+        idbPromise('categories', 'put', category);
+      });
+    } else if (!loading) {
+      idbPromise('categories', 'get').then((categories) => {
+        store.dispatch({
+          type: UPDATE_CATEGORIES,
+          categories: categories,
+        });
+      });
+    }
+  }, [categoryData, loading, store.dispatch]);
+
+  const handleClick = (id) => {
+    store.dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id,
+    });
+  };
+
+  return (
+    <div>
+      <h2>Choose a Category:</h2>
+      {categories.map((item) => (
+        <button
+          key={item._id}
+          onClick={() => {
+            handleClick(item._id);
+          }}
+        >
+          {item.name}
+        </button>
+      ))}
+      <button
+        onClick={() => {
+          handleClick('');
+        }}
+      >
+        All
+      </button>
+    </div>
+  );
+}
+
+export default CategoryMenu;
